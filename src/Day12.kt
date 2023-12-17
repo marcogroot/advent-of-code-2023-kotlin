f92cc61e63723e6f60738fdb971f23a5077fa8da
turdies <60631182+turdies@users.noreply.github.com>
2023-12-17 21:32:42 +1030

    .

--- src/Day12.kt
+++ src/Day12.kt
@@ -2,8 +2,7 @@ fun main() {
     fun part1(input: List<String>) : Int = input.sumOf { getCombinations(it) }
 
     fun part2(input: List<String>) : Long = input.sumOf {
-        val a = getCombinations2(it)
-        a
+        getCombinations2(it)
     }
 
 
@@ -33,30 +32,17 @@ private fun getCombinations(row: String) : Int {
     tailrec fun dfs(index: Int, valueIndex: Int, newSprocket: String) : Int {
         if(valueIndex == values.size) { return if (isValidSprocket(newSprocket)) 1 else 0 }
         if (index >= sprockets.length) return 0
-
         if (sprockets[index] == '.') return dfs(index+1, valueIndex, newSprocket)
-
         val currentValue = values[valueIndex]
-
         if (index+currentValue > sprockets.length) return 0
         val currentRange = sprockets.substring(index, index+currentValue)
-
         val temp = newSprocket.replaceRange(index, index+currentValue, "#".repeat(currentValue))
-
         return if (currentRange.all {it == '#' || it == '?'}) {
                 dfs(index+currentValue+1, valueIndex+1, temp) +
-                dfs(
-                    index+1,
-                    valueIndex,
-                    newSprocket
-                )
-        } else dfs(
-            index+1,
-            valueIndex,
-            newSprocket
+                dfs(index+1, valueIndex, newSprocket)
+        } else dfs(index+1, valueIndex, newSprocket
         )
     }
-
     return dfs(0, 0, sprockets)
 }
 
@@ -65,14 +51,10 @@ private fun getCombinations2(row: String) : Long {
 //    val sprocketsWithExcess = (splitRow.first().reversed() +'?').repeat(5)
 //    val sprocket = sprocketsWithExcess.take(sprocketsWithExcess.length-1)
 //    val values = (splitRow.last()+',').repeat(5).split(',').mapNotNull { if (it.isNotEmpty()) it.toInt() else null}.reversed()
-
     val splitRow = row.split(' ')
     val sprocket = splitRow.first().reversed()
     val values = splitRow.last().split(',').map { it.toInt() }.reversed()
-
     fun dfs(valueIndex: Int, newSprocket: String, fromJump: Boolean) : Long {
-        println("$newSprocket ; $valueIndex")
-
         if(valueIndex == values.size) {
             return 1
         }
@@ -81,16 +63,19 @@ private fun getCombinations2(row: String) : Long {
         if (newSprocket.isBlank()) return 0
         if (newSprocket.first() == '#' && fromJump) { return 0 }
         if (currentValue > newSprocket.length) { return 0 }
+        val m = ((values.size-valueIndex)*2)-2
+        if (newSprocket.length < m) return 0;
 
-        val nextResult = dfs(valueIndex, newSprocket.substring(1, 1+newSprocket.length-1), false)
-        val currentRange = sprocket.substring(0, 0+currentValue)
+        val nextResult = dfs(valueIndex, newSprocket.drop(1), false)
+        val currentRange = newSprocket.substring(0, 0+currentValue)
 
-        return if (currentRange.all {it == '#' || it == '?'}) {
-            println(currentRange)
-            println("ASDFASD ${newSprocket}")
-            val nextNumberResult = dfs(valueIndex+1, newSprocket.drop(currentValue), true)
-            nextNumberResult + nextResult
-        } else nextResult
+        val nextNumberResult = if (currentRange.all {it == '#' || it == '?'}) {
+            val opt1 = newSprocket.drop(currentValue)
+            val opt2 = if (opt1.isNotEmpty() && opt1.first() == '?') opt1.replaceFirst('?', '.') else opt1
+            dfs(valueIndex+1, opt2, true)
+        } else 0
+
+        return nextNumberResult+nextResult
     }
 
     return dfs(0, sprocket, false)
